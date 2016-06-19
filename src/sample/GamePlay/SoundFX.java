57c84493be6ee3527c0d30aa006d764d3e06e04d
ROSA <gojiradam@gmail.com>
2016-06-19 14:44:05 +0300

    SpaceShip

--- src/sample/GamePlay/SoundFX.java
+++ src/sample/GamePlay/SoundFX.java
@@ -1,4 +1,30 @@
 package sample.GamePlay;
 
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
+
 public class SoundFX {
+
+    public SoundFX(){
+        whilePlayGameMusic();
+    }
+
+
+    public void whilePlayGameMusic(){
+        AudioPlayer myBackgroundPlayer = AudioPlayer.player;
+        ContinuousAudioDataStream myLoop = null;
+        try {
+            AudioStream myBackgroundMusic = new AudioStream(
+                    getClass().getResourceAsStream("sample/Asset/SoundFX/Music/LevelOneMusic.wav"));
+            AudioData myData = myBackgroundMusic.getData();
+            myLoop = new ContinuousAudioDataStream(myData);
+        }catch(Exception error){
+            System.out.println("Music file error: " + error);
+        }
+        myBackgroundPlayer.start(myLoop);
+    }
+
 }
