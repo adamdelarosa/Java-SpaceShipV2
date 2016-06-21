d949aa91b1096fee076dadd91f79da77f3ee9d4a
ROSA <gojiradam@gmail.com>
2016-06-21 22:25:03 +0300

    addToScreenWeaponBox

--- src/sample/GamePlay/SpaceShip.java
+++ src/sample/GamePlay/SpaceShip.java
@@ -6,17 +6,17 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import sample.Main;
 
-public class SpaceShip  {
+public class SpaceShip {
 
     Main main;
     public HBox boxForShip = new HBox();
-    private static double W=500,H=800;
+    private static double W = 500, H = 800;
     private static String SPACESHIP = "sample/Asset/Image/spaceShip.png";
     private Image spaceShipImage;
     boolean running, goNorth, goSouth, goEast, goWest;
     ImageView nodeSpaceShip = new ImageView();
 
-    public SpaceShip(){
+    public SpaceShip() {
         shipAnimated();
         shipMovment();
         addToScreen();
@@ -28,28 +28,52 @@ public class SpaceShip  {
         nodeSpaceShip.setImage(spaceShipImage);
     }
 
-    public void shipMovment(){
+    public void shipMovment() {
 
         boxForShip.setFocusTraversable(true);
 
         boxForShip.setOnKeyPressed(event -> {
             switch (event.getCode()) {
-                case UP:    goNorth = true; break;
-                case DOWN:  goSouth = true; break;
-                case LEFT:  goWest  = true; break;
-                case RIGHT: goEast  = true; break;
-                case SHIFT: running = true; break;
-                case SPACE: new graph();System.out.println("hello"); break;
+                case UP:
+                    goNorth = true;
+                    break;
+                case DOWN:
+                    goSouth = true;
+                    break;
+                case LEFT:
+                    goWest = true;
+                    break;
+                case RIGHT:
+                    goEast = true;
+                    break;
+                case SHIFT:
+                    running = true;
+                    break;
+                case SPACE:
+                    new graph();
+                    System.out.println("hello");
+
+                    break;
             }
         });
 
         boxForShip.setOnKeyReleased(event -> {
             switch (event.getCode()) {
-                case UP:    goNorth = false; break;
-                case DOWN:  goSouth = false; break;
-                case LEFT:  goWest  = false; break;
-                case RIGHT: goEast  = false; break;
-                case SHIFT: running = false; break;
+                case UP:
+                    goNorth = false;
+                    break;
+                case DOWN:
+                    goSouth = false;
+                    break;
+                case LEFT:
+                    goWest = false;
+                    break;
+                case RIGHT:
+                    goEast = false;
+                    break;
+                case SHIFT:
+                    running = false;
+                    break;
             }
         });
 
@@ -60,19 +84,23 @@ public class SpaceShip  {
 
                 if (goNorth) dy -= 8;
                 if (goSouth) dy += 8;
-                if (goEast)  dx += 8;
-                if (goWest)  dx -= 8;
-                if (running) { dx *= 3; dy *= 3; }
+                if (goEast) dx += 8;
+                if (goWest) dx -= 8;
+                if (running) {
+                    dx *= 3;
+                    dy *= 3;
+                }
 
                 moveShipBy(dx, dy);
             }
         };
         timer.start();
     }
+
     private void moveShipBy(int dx, int dy) {
         if (dx == 0 && dy == 0) return;
 
-        final double cx = boxForShip.getBoundsInLocal().getWidth()  / 2;
+        final double cx = boxForShip.getBoundsInLocal().getWidth() / 2;
         final double cy = boxForShip.getBoundsInLocal().getHeight() / 2;
 
         double x = cx + boxForShip.getLayoutX() + dx;
@@ -82,7 +110,7 @@ public class SpaceShip  {
     }
 
     private void setSpaceShipPosition(double x, double y) {
-        final double cx = boxForShip.getBoundsInLocal().getWidth()  / 2;
+        final double cx = boxForShip.getBoundsInLocal().getWidth() / 2;
         final double cy = boxForShip.getBoundsInLocal().getHeight() / 2;
 
         if (x - cx >= 0 &&
@@ -93,7 +121,8 @@ public class SpaceShip  {
         }
 
     }
-    public void addToScreen(){
+
+    public void addToScreen() {
         boxForShip.getChildren().addAll(nodeSpaceShip);
         main.getRoot().getChildren().add(boxForShip);
 
